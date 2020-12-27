9d811d005d29c4198138013f820083831c0c4663
Kateryna Lutska <ket.lutska@gmail.com>
2020-12-27 19:00:27 +0200

    timer

--- src/js/refs.js
+++ src/js/refs.js
@@ -3,6 +3,7 @@ export default {
   hours: document.querySelector('[data-value="hours"]'),
   mins: document.querySelector('[data-value="mins"]'),
   secs: document.querySelector('[data-value="secs"]'),
+  selector: document.getElementById("timer-1"),
 };
 
 //console.log(refs.days.textContent);
--- src/js/timer.js
+++ src/js/timer.js
@@ -1,19 +1,22 @@
+// timer //
+
 import refs from "./refs.js";
 const { days, hours, min, secs } = refs;
 
+// == через  клас === //
+
 class CountdownTimer {
-  constructor(date, obj) {
+  constructor({ date, obj }) {
     this.date = date;
-    this.refs = obj;
-    console.log(this.refs.days);
+    this.selector = obj;
   }
   count() {
     let date = this.updateClockface(this.date - Date.now());
 
-    this.refs.days.textContent = date.days;
-    this.refs.hours.textContent = date.hours;
-    this.refs.mins.textContent = date.mins;
-    this.refs.secs.textContent = date.secs;
+    refs.days.textContent = date.days;
+    refs.hours.textContent = date.hours;
+    refs.mins.textContent = date.mins;
+    refs.secs.textContent = date.secs;
   }
   start() {
     setInterval(() => {
@@ -34,31 +37,14 @@ class CountdownTimer {
   }
 }
 
-let day = new Date("Dec 31, 2020");
-const timer = new CountdownTimer(day, refs);
-
-// const timer = new CountdownTimer({
-//   targetDate: new Date("Dec 31, 2020"),
-//   selector: "#timer-1",
-// });
+const timer = new CountdownTimer({
+  date: new Date("Dec 31, 2020"),
+  selector: "#timer-1",
+});
 
 timer.start();
 
-// function updateClockface(time) {
-//   const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-//   const hours = pad(
-//     Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-//   );
-//   const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-//   const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-//   return { days, hours, mins, secs };
-// }
-
-// function pad(value) {
-//   return String(value).padStart(2, "0");
-// }
-
-// == без класу ===
+// == без класу === //
 
 // const timer = {
 //   start() {
@@ -86,8 +72,6 @@ timer.start();
 //   refs.hours.textContent = hours;
 //   refs.mins.textContent = mins;
 //   refs.secs.textContent = secs;
-
-//   console.log(`${days}:${hours}:${mins}:${secs}`);
 // }
 
 // function pad(value) {
